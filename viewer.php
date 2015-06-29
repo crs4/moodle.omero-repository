bc59203e46dbc302194a69c6d347f1f1affde4ef
Marco Enrico Piras <kikkomep@crs4.it>
2015-06-29 12:16:05 +0200

    Refactored viewer.php controller:
      - parametric frameId
      - viewport controls are all encapsulated within an external javascript object
      - general cleaning

--- viewer.php
+++ viewer.php
@@ -22,9 +22,9 @@ $OMERO_SERVER = substr($OMERO_WEBGATEWAY, 0, strpos($OMERO_WEBGATEWAY, "/webgate
 
 // Read parameters from the actual URL
 $imageId = $_GET['id'];
-$width = $_GET['width'];
-$height = $_GET['height'];
-
+$frameId = $_GET['frame'];
+$width = $_GET['width'] ? !empty($_GET['width']) : "80%";
+$height = $_GET['height']; //? !empty($_GET['height']) : "100%";
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
@@ -32,212 +32,116 @@ $height = $_GET['height'];
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <META HTTP-EQUIV="Content-Style-Type" CONTENT="text/css">
-    <title>OMERO.web - embeded viewer</title>
+    <title>OMERO.web-viewer</title>
 
+    <!-- Third part CSS stylesheets *** -->
+
+    <!-- OmeroWeb CSS -->
+    <link rel="stylesheet" type="text/css" href="<?= $OMERO_SERVER ?>/static/omeroweb.viewer.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link href="/moodle/repository/omero/libs/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- overwrite default styles -->
     <style type="text/css">
         .viewport {
-            height: <?= $height ?>;
-            width: <?= $width ?>;
+            width: 100%;
+            height: 600px;
+            margin-top: auto;
+            margin-bottom: auto;
+            margin-right: 15px;
+            overflow: visible;
             padding: 5px;
         }
     </style>
 
-    <link rel="stylesheet" type="text/css" href="<?= $OMERO_SERVER ?>/static/omeroweb.viewer.min.css">
+
+    <!-- Third part libraries *** -->
+
+    <!-- JQuery -->
+    <script type="text/javascript" src="/moodle/repository/omero/libs/jquery/jquery-2.1.4.min.js"></script>
+    <!-- Bootstrap -->
+    <script type="text/javascript" src="/moodle/repository/omero/libs/bootstrap/js/bootstrap.min.js"></script>
+    <!-- OmeroViewer lib -->
     <script type="text/javascript" src="<?= $OMERO_SERVER ?>/static/omeroweb.viewer.min.js"></script>
+    <!-- OmeroViewerController -->
+    <script type="text/javascript" src="/moodle/repository/omero/viewer.js"></script>
+
+    <!--  Initialization script -->
     <script type="text/javascript">
 
         $(document).ready(function () {
             $.ajaxSettings.cache = false;
         });
 
-        var viewport;
+        // FIXME: just for debug
+        window.addEventListener("message", function (event) {
+            console.log("Message", event);
+        }, false);
 
-        var load_viewport = function () {
-            if (!viewport.viewportimg.get(0).refresh_rois) {
-                var options = {
-                    'width': viewport.loadedImg.size.width,
-                    'height': viewport.loadedImg.size.height,
-                    'json_url': '<?= $OMERO_SERVER ?>/webgateway/get_rois_json/' + viewport.loadedImg.id
-                };
-                if (viewport.loadedImg.tiles) {
-                    options['tiles'] = true;
-                }
+        // Get a reference to the actual omero_viewer_controller
+        var viewer_ctrl = omero_viewer_controller;
+        // Initialize the omero_viewer_controller
+        viewer_ctrl.init("<?= $OMERO_SERVER ?>", "<?= $frameId ?>", "<?= $imageId ?>");
 
-                viewport.viewportimg.roi_display(options);
-                viewport.viewportimg.get(0).setRoiZoom(viewport.viewportimg.get(0).getZoom());
-            }
-            else
-                console.log("Viewport already loaded");
-        };
-
-        var show_rois = function () {
-            var theT = viewport.getTPos();
-            var theZ = viewport.getZPos();
-
-            if (!viewport.viewportimg.get(0).show_rois) {
-                load_viewport();
-            }
-
-            // loads ROIs (if needed) and shows.
-            viewport.viewportimg.get(0).show_rois(theZ, theT);
-        };
-
-        var refresh_rois = function () {
-            console.log("embed_big_image_DEV refresh_rois method");
-            // re-plots the ROIs (if currently shown) for new Z and T position
-            if (viewport.viewportimg.get(0).refresh_rois) {
-                var theT = viewport.getTPos();
-                var theZ = viewport.getZPos();
-                var filter = viewport.viewportimg.get(0).get_current_rois_filter();
-                console.log("Current ROIs filter");
-                console.log(filter);
-                viewport.viewportimg.get(0).refresh_rois(theZ, theT, filter);
-            }
-        };
-
-        var hide_rois = function () {
-            // hides the display of ROIs.
-            if (viewport.viewportimg.get(0).hide_rois) {
-                viewport.viewportimg.get(0).hide_rois();
-            }
-        };
-
-        var show_scalebar = function () {
-            if (!viewport.viewportimg.get(0).show_scalebar) {
-                // if the Scalebar plugin has not been initialised (method not available...) init and load Scalebar...
-                var options = {
-                    'pixSizeX': viewport.getPixelSizes().x,
-                    'imageWidth': viewport.getSizes().width
-                };
-                if (viewport.loadedImg.tiles) {
-                    options['tiles'] = true;
-                }
-                viewport.viewportimg.scalebar_display(options);
-            }
-
-            viewport.viewportimg.get(0).setScalebarZoom(viewport.getZoom() / 100);
-            viewport.viewportimg.get(0).show_scalebar();
-
-        };
-
-        var hide_scalebar = function () {
-            viewport.viewportimg.get(0).hide_scalebar();
-        };
-
-        var _imageLoad = function (ev, viewport) {
-
-            /**
-             * This function is called when an image is initially loaded.
-             * This is the place to sync everything; rendering model, quality, channel buttons, etc.
-             */
-
-            /* load metadata */
-            $('#image-name').html(viewport.loadedImg.meta.imageName);
-
-            /* enable scalebar */
-            tmp = viewport.getPixelSizes();
-            if (tmp.x !== 0) {
-                $("#viewport-scalebar").prop("disabled", false);
-                $("#viewport-scalebar").prop("checked", true);
-                show_scalebar();
-            }
-
-            /**
-             * Attach functions to the click event on specific buttons
-             */
-            $("#viewport-show-rois").click(function () {
-                show_rois();
-            });
-            $("#viewport-hide-rois").click(function () {
-                hide_rois();
-            });
-            $("#viewport-add-shapes").click(function () {
-                add_external_shapes();
-            });
-            $("#viewport-remove-shape-1").click(function () {
-                remove_external_shape("X1", 1);
-            });
-            $("#viewport-remove-shape-2").click(function () {
-                remove_external_shape("X2", 1);
-            });
-
-
-            /**
-             * Attach functions to the click event on specific buttons
-             */
-
-                // 'Scalebar' checkbox to left of image
-            $("#viewport-scalebar").change(function () {
-                if (this.checked) {
-                    show_scalebar();
-                } else {
-                    hide_scalebar();
-                }
-            });
-        };
-
-        var instant_zoom = function (e, percent) {
-            if (viewport.viewportimg.get(0).setRoiZoom) {
-                viewport.viewportimg.get(0).setRoiZoom(percent);
-            }
-            if (viewport.viewportimg.get(0).setScalebarZoom) {
-                viewport.viewportimg.get(0).setScalebarZoom(percent / 100);
-            }
-        };
-
-        var add_external_shapes = function () {
-            if (!viewport.viewportimg.get(0).show_rois) {
-                load_viewport();
-            }
-
-            var r1 = viewport.viewportimg.get_ome_rectangle(2000, 2000, 4000, 4000, 0, 0);
-            var r2_shape = viewport.viewportimg.get_shape_config(undefined, undefined, undefined,
-                undefined, "#0000ff");
-            var r2 = viewport.viewportimg.get_ome_rectangle(3000, 3000, 4000, 4000, 0, 0,
-                undefined, r2_shape);
-
-            var vimg = viewport.viewportimg.get(0);
-            vimg.push_shape("X1", 1, r1, false);
-            vimg.push_shape("X2", 1, r2, true);
-        };
-
-        var remove_external_shape = function (roi_id, shape_id) {
-            var vimg = viewport.viewportimg.get(0);
-            vimg.remove_shape(roi_id, shape_id, true);
-        };
-
-        $(document).ready(function () {
-
-            /* Prepare the viewport */
-            viewport = $.WeblitzViewport($("#viewport"), "<?= $OMERO_SERVER ?>/webgateway/", {
-                'mediaroot': "<?= $OMERO_SERVER ?>/static/"
-            });
-
-            /* Async call needs loading */
-            viewport.bind('imageLoad', _imageLoad);
-            /* Bind zoomimg action to the ROIs */
-            viewport.bind('instant_zoom', instant_zoom);
-
-            /* Load the selected image into the viewport */
-            viewport.load(<?=$imageId?>);
-
-        });
     </script>
 
 </head>
-<body style="background: white; padding: 10px; border: none; ">
+
+<body style="background: white; padding: 10px; border: none;">
 
 <label for="viewport-scalebar">Scalebar</label>
 <input id="viewport-scalebar" type="checkbox" disabled/>
 
+<!-- FIXME: the following are example controls; the real controls have to be defined -->
 <button id="viewport-show-rois" title="Show ROIs">Show ROIs</button>
 <button id="viewport-hide-rois" title="Hide ROIs">Hide ROIs</button>
-
 <button id="viewport-add-shapes" title="Add shapes">Add External Shapes</button>
 <button id="viewport-remove-shape-1" title="Remove shape 1">Remove External Shape #1</button>
 <button id="viewport-remove-shape-2" title="Remove shape 2">Remove External Shape #2</button>
 
-<div id="viewport" class="viewport" style="margin:10px;"></div>
+<!-- container for the omero-viewer viewpoer -->
+<div id="viewport" class="viewport"></div>
+
+<!-- FIXME: Static table example: the table has to be dynamically generated -->
+<div id="rois-table-container" class="panel panel-default" style="margin-top: 20px;font-size: 12pt;">
+
+    <!-- Default panel contents -->
+    <div class="panel-heading">ROI Inspector</div>
+
+    <!-- Table -->
+    <table id="rois-table" class="table" data-toggle="table">
+        <thead>
+        <tr>
+            <th>Id</th>
+            <th>Shape</th>
+            <th>Color</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+        </tr>
+        </tbody>
+    </table>
+</div>
 
 </body>
 </html>
