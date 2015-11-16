8851cee3186f9a5d281e52b852d2ef28b384e118
Marco Enrico Piras <kikkomep@crs4.it>
2015-11-16 18:40:44 +0100

    Fixed properties names as private

--- viewer/viewer-controller.js
+++ viewer/viewer-controller.js
@@ -21,20 +21,19 @@ ctrl.init = function (image_server, frame_id, viewport_id, rois_table_id, roi_sh
     var me = image_viewer_controller;
 
     // register the actual initialization parameters
-    me.image_server = image_server;
-    me.frame_id = frame_id;
-    me.viewport_id = viewport_id;
-    me.rois_table_id = rois_table_id;
-    me.image_id = image_id;
-    me.image_params = image_params;
-    me.visible_rois = visible_rois; // && visible_rois.length > 0 ? visible_rois.split(",") : [];
+    me._image_server = _image_server;
+    me._frame_id = _frame_id;
+    me._viewport_id = _viewport_id;
+    me._rois_table_id = _rois_table_id;
+    me._image_id = _image_id;
+    me._image_params = image_params;
+    me._visible_rois = visible_rois; // && visible_rois.length > 0 ? visible_rois.split(",") : [];
     me._visible_roi_shape_list = [];
-    me.roi_shape_thumb_popup_id = roi_shape_thumb_popup_id;
+    me._roi_shape_thumb_popup_id = roi_shape_thumb_popup_id;
     me._show_roi_table = show_roi_table;
-    me.window = window;
 
     // set frame reference
-    me._frame = window.parent.document.getElementById(me.frame_id);
+    me._frame = window.parent.document.getElementById(me._frame_id);
 
     // log controller initialization status
     console.log("image_viewer_controller initialized!!!");
