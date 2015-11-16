ef7eec21d5e458243bc350bb39f1fe0503957dce
Marco Enrico Piras <kikkomep@crs4.it>
2015-11-16 19:55:32 +0100

    Added method to bind the concrete viewer; setted the default behaviour

--- viewer/viewer-controller.js
+++ viewer/viewer-controller.js
@@ -6,7 +6,7 @@
 image_viewer_controller = {};
 
 // internal shortcut for the controller instance
-var ctrl = image_viewer_controller;
+var me = image_viewer_controller;
 
 /**
  * Initialize the controller of the actual image viewer
@@ -15,29 +15,55 @@ var ctrl = image_viewer_controller;
  * @param frame_id the frame containing the viewer if it exists
  * @param image_id the image of the image to immediately view after the initialization
  */
-ctrl.init = function (image_server, frame_id, viewport_id, rois_table_id, roi_shape_thumb_popup_id,
-                      image_id, show_roi_table, image_params, visible_rois) {
-
-    var me = image_viewer_controller;
+me.init = function (image_server, frame_id, viewport_id, rois_table_id, roi_shape_thumb_popup_id,
+                    image_id, show_roi_table, image_params, visible_rois) {
 
     // register the actual initialization parameters
-    me._image_server = _image_server;
-    me._frame_id = _frame_id;
-    me._viewport_id = _viewport_id;
-    me._rois_table_id = _rois_table_id;
-    me._image_id = _image_id;
+    me._image_server = image_server;
+    me._frame_id = frame_id;
+    me._viewport_id = viewport_id;
+    me._rois_table_id = rois_table_id;
+    me._image_id = image_id;
     me._image_params = image_params;
     me._visible_rois = visible_rois; // && visible_rois.length > 0 ? visible_rois.split(",") : [];
     me._visible_roi_shape_list = [];
     me._roi_shape_thumb_popup_id = roi_shape_thumb_popup_id;
     me._show_roi_table = show_roi_table;
 
+
     // set frame reference
     me._frame = window.parent.document.getElementById(me._frame_id);
 
+    // TODO: to change with the controller initialization
+    if (!me._viewer) {
+        console.warn("ViewerController not initialized!!!");
+    }
+
+    // TODO: add param to change the default behaviour
+    if (me._viewer) {
+        me.showImage();
+    }
+
     // log controller initialization status
-    console.log("image_viewer_controller initialized!!!");
+    console.log("image_viewer_controller initialized!!!", me);
 };
+
+
+/**
+ * Registers a reference to the concrete ImageViewerController
+ * @param viewer
+ */
+me.setViewer = function (viewer) {
+    me._viewer = viewer;
+};
+
+/**
+ * Shows the image
+ */
+me.showImage = function () {
+    me._viewer.buildViewer();
+};
+
 
 
 
