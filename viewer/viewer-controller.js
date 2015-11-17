13a9192a318a1e5112d513640348b10fe9c14d65
Marco Enrico Piras <kikkomep@crs4.it>
2015-11-17 19:08:13 +0100

    Restored an initial version of ROI table (not complete)

--- viewer/viewer-controller.js
+++ viewer/viewer-controller.js
@@ -155,6 +155,221 @@ ImageViewerController.prototype._removeVisibleRoiShapes = function (roi_ids) {
         }
     }
 }
+
+
+/**
+ * Render the ROI table
+ *
+ * @param dataSet
+ * @private
+ */
+ImageViewerController.prototype.renderRoisTable = function (dataSet) {
+
+    var me = this;
+
+    console.log("Rendering table started .... ");
+
+    var roi_table = $('#rois-table');
+    roi_table.dataTable({
+        "data": dataSet,
+        "cell-border": true,
+        "pageLength": 10,
+        "lengthMenu": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        "columns": [
+            {"title": "ID", data: "id", "width": "20px", "className": "dt-head-center dt-body-center"},
+            {"title": "Z", data: "shapes[0].theZ", "width": "20px", "className": "dt-head-center dt-body-center"},
+            {"title": "T", data: "shapes[0].theT", "width": "20px", "className": "dt-head-center dt-body-center"},
+            {
+                "title": "Description",
+                "data": "shapes[0].textValue",
+                "className": "roi-description dt-head-center dt-body-left"
+            },
+            {
+                "title": "Preview",
+                "data": "shapes[0].id",
+                "className": "dt-head-center dt-body-center",
+                "width": "100px",
+                "render": function (data, type, row) {
+                    if (type === 'display') {
+                        return '<div class="shape-thumb-container" style=""><img src=" ' + me.omero_server +
+                            '/webgateway/render_shape_thumbnail/0' + data + '/?color=f00" ' +
+                            'id="' + data + '_shape_thumb" ' +
+                            'class="roi_thumb shape_thumb" ' +
+                            'style="vertical-align: top;"  ' +
+                            'color="f00" width="150px" height="150px" /></div>';
+                    }
+                    return data;
+                }
+            },
+            {
+                "title": "Visibility",
+                "data": "id",
+                "className": "dt-head-center dt-body-center",
+                "width": "20px",
+                "render": function (data, type, row) {
+                    if (type === 'display') {
+                        return '<input id="visibility_selector_' + data + '" ' +
+                            (me._visible_rois.indexOf(data.toString()) != -1 ? " checked " : "") +
+                            ' type="checkbox" class="editor-active"  style="width: 20px">';
+                    }
+                    return data;
+                }
+            }
+        ],
+
+        rowCallback: function (row, data) {
+            // Set the checked state of the checkbox in the table
+            console.log("DATA", row, data, data[4], data[5]);
+            $('input.editor-active', row).prop('checked', data.visibility);
+        }
+
+        //"drawCallback": function ( settings ) {
+        //    var api = this.api();
+        //    var rows = api.rows( {page:'current'} ).nodes();
+        //    var last=null;
+        //
+        //    api.column(2, {page:'current'} ).data().each( function ( group, i ) {
+        //        if ( last !== group ) {
+        //            $(rows).eq( i ).before(
+        //                '<tr class="group"><td colspan="5">'+group+'</td></tr>'
+        //            );
+        //
+        //            last = group;
+        //        }
+        //    } );
+        //}
+    });
+
+
+    // Handle row selection, i.e., selection of the corresponding ROI shape
+    $('#rois-table tbody').on('click', 'tr', function (event) {
+        alert("Click!");
+        var data_table = roi_table.DataTable();
+        var selected_roi_shape = data_table.row(this).data();
+        var selected = true;
+        var target = (event.target || event.srcElement);
+        if ($(this).hasClass('selected') && target.type != "checkbox") {
+            // Deselects an already selected row:
+            // skips the deselection if the click has been triggered by a checkbox
+            selected = false;
+            $(this).removeClass('selected');
+            console.log("Deselected ROI shape: " + selected_roi_shape.id, selected_roi_shape);
+        } else {
+            // Selected a table row
+            data_table.$('tr.selected').removeClass('selected');
+            $(this).addClass('selected');
+            console.log("Selected ROI shape: " + selected_roi_shape.id, selected_roi_shape);
+
+            console.log(selected_roi_shape);
+            alert("XXXXX");
+
+            me._addVisibleRoiShapes(selected_roi_shape.id);
+            //me._handleShapeRowClick(selected_roi_shape.shapes[0]);
+        }
+
+        window.dispatchEvent(new CustomEvent(
+            "roiShape" + (selected ? "Selected" : "Deselected"),
+            {
+                detail: {
+                    id: selected_roi_shape.id + "@" + selected_roi_shape.shapes[0].id,
+                    roiId: selected_roi_shape.id,
+                    shapeId: selected_roi_shape.shapes[0].id,
+                    detail: selected_roi_shape
+                },
+                bubbles: true
+            })
+        );
+    });
+
+
+    // Handle the selection of a given row (image)
+    roi_table.on('change', function (event) {
+        //var me = this;
+        var target = (event.target || event.srcElement);
+        var selectorId = target.id;
+        if (selectorId) {
+            var roiId = selectorId.match(/[0-9]+/);
+            if (roiId) {
+
+                var checked = $("#" + selectorId).is(":checked");
+                console.log("Changed visibility of " + roiId + ": " + (checked ? "display" : "hidden"));
+
+                var selected_roi_info = $.grep(me._current_roi_list, function (e) {
+                    return e.id == roiId;
+                });
+                if (selected_roi_info.length > 0) {
+                    // update var to point to the selected ROI
+                    selected_roi_info = selected_roi_info[0];
+                    // prepare ROI shape info
+                    var selected_shape_info = {};
+                    selected_shape_info[selected_roi_info.id] = [selected_roi_info.shapes[0]]; // FIXME: a better mechanism for shape selection
+                    //checked ? me._show_rois(selected_shape_info) : me.hide_rois(selected_roi_info);
+
+                    checked ? me.showRoi(selected_roi_info) : me.hideRoi(selected_roi_info);
+
+
+                    // Notifies the event
+                    window.dispatchEvent(new CustomEvent(
+                        "roiVisibilityChanged",
+                        {
+                            detail: {
+                                id: selected_roi_info.id + "@" + selected_roi_info.shapes[0].id,
+                                roiId: selected_roi_info.id,
+                                shapeId: selected_roi_info.shapes[0].id,
+                                detail: selected_roi_info,
+                                visible: checked
+                            },
+                            bubbles: true
+                        })
+                    );
+                }
+                // FIXME: it is just an example of event notification
+                //window.postMessage({
+                //    roiId: roiId,
+                //    event: "roi_visibility_changed",
+                //    visibility: (checked ? "display" : "hidden")
+                //}, "*");
+            }
+        }
+    });
+
+    // now bind mouseover: enable/disable shape thumbnail popup
+    var roi_thumb_popup = $("#" + me.roi_shape_thumb_popup_id);
+    roi_thumb_popup.updateShapeThumbnails = function () {
+        $('.roi_thumb').hover(function (e) {
+            roi_thumb_popup.attr('src', $(this).attr('src')).show();
+        }, function (e) {
+            roi_thumb_popup.hide();
+        });
+
+        $('.roi_thumb').mousemove(function (e) {
+            roi_thumb_popup.css({'left': e.pageX + 5, 'top': e.pageY + 5})
+        });
+    };
+
+    // Resize after pagination FIXME: is really needed?
+    roi_table.on('page.dt', function () {
+        var info = $('#rois-table').DataTable().page.info();
+        $('#pageInfo').html('Showing page: ' + info.page + ' of ' + info.pages);
+        me._resize();
+        roi_thumb_popup.updateShapeThumbnails();
+    });
+
+    // Resize after every draw
+    roi_table.on('draw.dt', function () {
+        console.log('Redraw occurred at: ' + new Date().getTime());
+        me._resize();
+        roi_thumb_popup.updateShapeThumbnails();
+    });
+
+    // call resize
+    me._resize();
+    // first roi_thumb_popup initialization
+    roi_thumb_popup.updateShapeThumbnails();
+    console.log("Rendering table stopped .... ");
+};
+
+
 /**
  * Resize the viewer
  *
