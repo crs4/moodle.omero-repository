8a081a2893628822a2bd466043473c8832914886
Marco Enrico Piras <kikkomep@crs4.it>
2015-11-24 11:31:26 +0100

    Updated version number

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015111100;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2015051102;        // Requires this Moodle version
+$plugin->version   = 2015112400;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2015051100;        // Requires this Moodle version
 $plugin->component = 'repository_omero'; // Full name of the plugin (used for diagnostics)
