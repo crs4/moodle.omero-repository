27ff5cd1d91989480127146e1810509922a1ad6b
Marco Enrico Piras <kikkomep@crs4.it>
2016-02-09 10:10:40 +0100

    Refactoring

--- lang/en/repository_omero.php
+++ lang/en/repository_omero.php
@@ -22,20 +22,28 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+# general
 $string['pluginname'] = 'Omero';
-$string['configplugin'] = 'Omero Repository Configuration';
+$string['configplugin'] = 'Configurazione Repository Omero';
 $string['omero_restendpoint'] = 'Ome-Seadragon REST API EndPoint';
+
+# API authentication and authorization
 $string['apikey'] = 'omero API key';
 $string['omero'] = 'omero';
 $string['secret'] = 'omero secret';
-$string['instruction'] = 'You can get your API Key and secret from <a href="http://www.omero.com/developers/apps">omero developers</a>. When setting up your key please select "Full omero" as the "Access level".';
-$string['omero:view'] = 'View a omero folder';
-$string['logoutdesc'] = '(Logout when you finish using omero)';
-$string['current_image'] = "Current image";
-$string['choose_image'] = "Choose an image";
+# TODO: to be updated
+$string['instruction'] = 'You can get your API Key and secret from <pre>OMERO API Endpoint>/apps</pre>.';
+
+# cache definition
+$string['cachedef_repository_info_cache'] = "Session cache for queries to the OmeSeadraon Rest API";
+$string['cachedef_thumbnail_cache'] = "Application cache to optimize retrieval of omeseadragon thumbnails";
+
 # cache settings
 $string['cachelimit'] = 'Cache limit';
 $string['cachelimit_info'] = 'Enter the maximum size of files (in bytes) to be cached on server for omero aliases/shortcuts. Cached files will be served when the source is no longer available. Empty value or zero mean caching of all files regardless of size.';
-# cache description
-$string['cachedef_repository_info_cache'] = "Session cache for queries to the OmeSeadraon Rest API";
-$string['cachedef_thumbnail_cache'] = "Application cache to optimize retrieval of omeseadragon thumbnails";
+
+# UI strings
+$string['current_image'] = "Current image";
+$string['choose_image'] = "Choose an image";
+$string['omero:view'] = 'View a omero folder';
+$string['logoutdesc'] = '(Logout when you finish using omero)';
