c087fe2f85040876af6266f2d17e4c923d77621f
Marco Enrico Piras <kikkomep@crs4.it>
2016-02-10 10:42:49 +0100

    Updated doc and copyright

--- db/caches.php
+++ db/caches.php
@@ -1,13 +1,30 @@
 <?php
+// Copyright (c) 2015-2016, CRS4
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 /**
- * Created by PhpStorm.
- * User: kikkomep
- * Date: 02/02/16
- * Time: 22:53
+ * Cache definitions
+ *
+ * @package    repository_omero
+ * @copyright  2015-2016 CRS4
+ * @licence    https://opensource.org/licenses/mit-license.php MIT licence
  */
-
-
-// moodle/mod/myplugin/db/caches.php
 $definitions = array(
     'thumbnail_cache' => array(
         'mode' => cache_store::MODE_APPLICATION
--- thumbnail.php
+++ thumbnail.php
@@ -1,14 +1,31 @@
 <?php
 
+// Copyright (c) 2015-2016, CRS4
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 /**
  * This script displays one thumbnail of the image in current user's omero.
- * If {@link repository_omero::send_thumbnail()} can not display image
- * the default 64x64 filetype icon is returned
  *
+ * @since      Moodle 2.0
  * @package    repository_omero
- * @copyright  2015 CRS4
- * @author
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright  2015-2016 CRS4
+ * @licence    https://opensource.org/licenses/mit-license.php MIT licence
  */
 
 require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
