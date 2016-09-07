91a8adde2ba9d1de571abafd02ca1701c3ad6116
Marco Enrico Piras <kikkomep@crs4.it>
2016-09-07 10:17:02 +0200

    Added new configuration strings

--- lang/en/repository_omero.php
+++ lang/en/repository_omero.php
@@ -30,14 +30,16 @@
 # general
 $string['pluginname'] = 'Omero';
 $string['configplugin'] = 'Configurazione Repository Omero';
-$string['omero_restendpoint'] = 'Ome-Seadragon REST API EndPoint';
+$string['omero_server'] = 'OmeSeadragon Server';
+$string['omero_restendpoint'] = 'OmeSeadragon REST API EndPoint';
 
 # API authentication and authorization
-$string['apikey'] = 'omero API key';
 $string['omero'] = 'omero';
-$string['secret'] = 'omero secret';
+$string['apiversion'] = 'OmeSeadragon API Version';
+$string['apikey'] = 'OmeSeadragon API key';
+$string['apisecret'] = 'OmeSeadragon API secret';
 # TODO: to be updated
-$string['instruction'] = 'You can get your API Key and secret from <pre>OMERO API Endpoint>/apps</pre>.';
+$string['instruction'] = 'You can get your API Key and secret from: <pre>OMERO API Endpoint>/apps</pre>';
 
 # cache definition
 $string['cachedef_repository_info_cache'] = "Session cache for queries to the OmeSeadraon Rest API";
--- lang/it/repository_omero.php
+++ lang/it/repository_omero.php
@@ -29,12 +29,14 @@
 # general
 $string['pluginname'] = 'Omero';
 $string['configplugin'] = 'Configurazione Repository Omero';
-$string['omero_restendpoint'] = 'Ome-Seadragon REST API EndPoint';
+$string['omero_server'] = 'OmeSeadragon Server';
+$string['omero_restendpoint'] = 'OmeSeadragon REST API EndPoint';
 
 # API authentication and authorization
-$string['apikey'] = 'omero API key';
 $string['omero'] = 'omero';
-$string['secret'] = 'omero secret';
+$string['apiversion'] = 'OmeSeadragon API Version';
+$string['apikey'] = 'OmeSeadragon API key';
+$string['apisecret'] = 'OmeSeadragon API secret';
 # TODO: to be updated
 $string['instruction'] = 'E\' possibile ottenere l\'<em>API Key</em> e l\'<em>API secret</em>em> da <pre>OMERO API Endpoint>/apps</pre>.';
 
